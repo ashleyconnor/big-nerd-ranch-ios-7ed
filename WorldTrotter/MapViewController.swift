55c9902aef6115897cdab3168459d30151ce68f0
Ashley Connor <ash.connor@gmail.com>
2020-05-12 00:25:11 -0400

    Chapter 6 Silver

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 import MapKit
 
-class MapViewController : UIViewController {
+class MapViewController : UIViewController, MKMapViewDelegate {
     
     var mapView: MKMapView!
+    var locationManager: CLLocationManager!
     
     override func loadView() {
         mapView = MKMapView()
+        mapView.delegate = self
         
         view = mapView
         
@@ -28,7 +30,7 @@ class MapViewController : UIViewController {
         view.addSubview(segementedControl)
         
         let topConstraint = segementedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
-        
+
         let margins = view.layoutMarginsGuide
         let leadingConstraint = segementedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
         let trailingConstraint = segementedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
@@ -60,8 +62,19 @@ class MapViewController : UIViewController {
         labelLeadingConstraint.isActive = true
         togglelTopConstraint.isActive = true
         toggleLeadingConstraint.isActive = true
+
+        locationManager = CLLocationManager()
+        locationManager.requestWhenInUseAuthorization()
+
+        mapView.showsUserLocation = true
     }
-    
+
+
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        let viewRegion = MKCoordinateRegion(center: userLocation.coordinate, latitudinalMeters: 200, longitudinalMeters: 200)
+        mapView.setRegion(viewRegion, animated: true)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         print("MapViewController loaded its view.")
